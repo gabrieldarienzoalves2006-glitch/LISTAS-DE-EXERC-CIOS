aac732e9f197d254ed711d06c28702bcf1a37530
GABRIEL DARIENZO ALVES RESENDE <gabrieldarienzoalves2006@gmail.com>
2026-09-15 14:41:18 -0300

    Add files via upload

---
+++ LISTA 06/Nome_Banco.java
@@ -0,0 +1,13 @@
+public class Nome_Banco {
+    Nome_BAnco -> javaBank;
+    Taxa_Saque -> 5.0;
+
+    registrarNovaConta(Conta conta) {
+     
+        // Lógica para registrar uma nova conta no banco
+
+        
+    }
+
+
+}
